65196b39f216641bfd87dc004d6e3a0845a18313
junior <ae_junior@yahoo.com>
2020-01-12 17:26:41 +0700

    cuman test doang kok

---
+++ hasilTs3.py
@@ -0,0 +1,106 @@
+Using TensorFlow backend.
+WARNING:tensorflow:From C:\Users\Acer Pc\AppData\Local\Programs\Python\Python37\lib\site-packages\tensorflow\python\framework\op_def_library.py:263: colocate_with (from tensorflow.python.framework.ops) is deprecated and will be removed in a future version.
+Instructions for updating:
+Colocations handled automatically by placer.
+WARNING:tensorflow:From C:\Users\Acer Pc\AppData\Local\Programs\Python\Python37\lib\site-packages\keras\backend\tensorflow_backend.py:3445: calling dropout (from tensorflow.python.ops.nn_ops) with keep_prob is deprecated and will be removed in a future version.
+Instructions for updating:
+Please use `rate` instead of `keep_prob`. Rate should be set to `rate = 1 - keep_prob`.
+WARNING:tensorflow:From C:\Users\Acer Pc\AppData\Local\Programs\Python\Python37\lib\site-packages\tensorflow\python\ops\math_ops.py:3066: to_int32 (from tensorflow.python.ops.math_ops) is deprecated and will be removed in a future version.
+Instructions for updating:
+Use tf.cast instead.
+data/test_image/kaoskaki_1.jpg
+
+Predicted: Kaos Kaki
+
+
+
+data/test_image/kaoskaki_2.jpg
+
+Predicted: Kaos Kaki
+
+
+
+data/test_image/kaoskaki_3.jpg
+
+Predicted: Kaos Kaki
+
+
+
+data/test_image/kaoskaki_4.jpg
+
+Predicted: Kaos Kaki
+
+
+
+data/test_image/kaoskaki_5.jpg
+
+Predicted: Kaos Kaki
+
+
+
+data/test_image/sandals_1.jpg
+
+Predicted: Sendal
+
+
+
+data/test_image/sandals_2.jpg
+
+Predicted: Sendal
+
+
+
+data/test_image/sandals_3.jpg
+
+Predicted: Sendal
+
+
+
+data/test_image/sandals_4.jpg
+
+Predicted: Kaos Kaki
+
+
+
+data/test_image/sandals_5.jpg
+
+Predicted: Sendal
+
+
+
+data/test_image/sepatu_1.jpg
+
+Predicted: Sepatu
+
+
+
+data/test_image/sepatu_2.jpg
+
+Predicted: Sepatu
+
+
+
+data/test_image/sepatu_3.jpg
+
+Predicted: Sepatu
+
+
+
+data/test_image/sepatu_4.jpg
+
+Predicted: Sepatu
+
+
+
+data/test_image/sepatu_5.jpg
+
+Predicted: Sepatu
+
+
+
+Execution Time:
+
+30.50403881072998
+
+seconds
+
